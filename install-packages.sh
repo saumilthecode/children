a405f685ce6585300392bb36bbc286d79eaa96af
potato dog man <saumil707@gmail.com>
2024-07-15 21:20:52 +0800

    push all

--- install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt install build-essential libssl-dev libffi-dev python3-dev libhdf5-dev
-sudo apt install -y python3-pip
-sudo pip3 install jupyter --break-system-packages
-sudo pip3 install djitellopy --break-system-packages
-sudo pip3 install tflite_runtime --break-system-packages
-sudo pip3 install pygame --break-system-packages
